b007296d3bc766e112747104bfe7547e80aa5732
Zappat0n <albarros1973@gmail.com>
2021-03-10 23:31:41 +0100

    Edit projects content

---
+++ src/dist/projects.dev.js
@@ -0,0 +1,53 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var projects = [{
+  title: 'Elan manager app',
+  image: './images/elan_manager_app.png',
+  technologies: 'Ruby | Rails | Git | Gitflow | Rubocop | Styleint | VS Code | Devise | Bootstrap',
+  github: 'https://github.com/Zappat0n/elan_manager_app',
+  description: "Mobile web application with a score recording system for a school developed in Rails."
+}, {
+  title: 'The extortion',
+  image: './images/the_extortion.png',
+  technologies: 'JavaScript | Node package manager | Phaser 3 | Jest | HTML5 | SASS(SCSS)',
+  github: 'https://github.com/Zappat0n/The_extortion',
+  live: 'https://zappat0n.github.io/The_extortion/',
+  description: "Þrymr has stolen Thor's hammer to extort the gods into giving him Freyja as his wife. Would you travel to Jötunheimr and climb the walls of Útgarðar to help Thor recover his hammer?"
+}, {
+  title: 'Elan manager',
+  image: './images/elan_manager.png',
+  technologies: 'Java | MySql | Google API | Gradle | Git | Gitflow | IntelliJ IDEA',
+  github: 'https://github.com/Zappat0n/elan_manager',
+  description: 'An application developed using Java, MySql, and Google Drive to manage a Montessori British School.'
+}, {
+  title: 'Tic-tac-toe game',
+  image: './images/Screenshot-tic-tac-toe.png',
+  github: 'https://github.com/Zappat0n/tic-tac-toe',
+  technologies: 'Ruby | Git | Gitflow | Rubocop | VS Code',
+  description: 'A tic-tac-toe game for the terminal developed in Ruby'
+}, {
+  title: 'Scraper from Yahoo Finance!',
+  image: './images/Screenshot-scrapper.png',
+  github: 'https://github.com/Zappat0n/stock_scraper',
+  technologies: 'Ruby | Git | Gitflow | Rubocop | VS Code',
+  description: 'An application developed with Ruby to get financial information in real-time from Yahoo Finance!.'
+}, {
+  title: 'Elan Montessori Event website',
+  image: './images/Screenshot-elan-event.png',
+  technologies: 'HTML | CSS | Bootstrap | Git | Gitflow | StileInt | WebHint | VS Code',
+  github: 'https://github.com/Zappat0n/education2020',
+  description: 'This a responsive conference website. It is for a Positive Discipline education event that took place in Murcia (Spain) in 2020.'
+}, {
+  title: 'Weather app',
+  image: './images/weather_app.png',
+  technologies: 'HTML5 | SASS(SCSS) | JavaScript | Node package manager',
+  github: 'https://github.com/Zappat0n/weather_app',
+  live: 'https://zappat0n.github.io/weather_app/',
+  description: "Mobile web application developed in JavaScript that gets information from OpenWeatherMap API. It allows you to see the current weather in any location."
+}];
+var _default = projects;
+exports["default"] = _default;
--- src/projects.js
+++ src/projects.js
@@ -2,14 +2,14 @@ const projects = [
   {
     title: 'Elan manager app',
     image: './images/elan_manager_app.png',
-    technologies: 'Ruby | Rails | Git | Gitflows | Rubocop | Styleint | VS Code | Devise | Bootstrap',
+    technologies: 'Ruby | Rails | Git | Gitflow | Rubocop | Styleint | VS Code | Devise | Bootstrap',
     github: 'https://github.com/Zappat0n/elan_manager_app',
     description: "Mobile web application with a score recording system for a school developed in Rails.",
   },
   {
     title: 'The extortion',
     image: './images/the_extortion.png',
-    technologies: 'HTML5 | SASS(SCSS) | JavaScript | Node package manager | Phaser 3 |    Jest',
+    technologies: 'JavaScript | Node package manager | Phaser 3 | Jest | HTML5 | SASS(SCSS)',
     github: 'https://github.com/Zappat0n/The_extortion',
     live: 'https://zappat0n.github.io/The_extortion/',
     description: "Þrymr has stolen Thor's hammer to extort the gods into giving him Freyja as his wife. Would you travel to Jötunheimr and climb the walls of Útgarðar to help Thor recover his hammer?",
@@ -17,19 +17,21 @@ const projects = [
   {
     title: 'Elan manager',
     image: './images/elan_manager.png',
-    technologies: 'Java | MySql | Google API | Gradle | Git | GitFlow | IntelliJ IDEA',
+    technologies: 'Java | MySql | Google API | Gradle | Git | Gitflow | IntelliJ IDEA',
     github: 'https://github.com/Zappat0n/elan_manager',
     description: 'An application developed using Java, MySql, and Google Drive to manage a Montessori British School.',
   },
   {
     title: 'Tic-tac-toe game',
     image: './images/Screenshot-tic-tac-toe.png',
+    github: 'https://github.com/Zappat0n/tic-tac-toe',
     technologies: 'Ruby | Git | Gitflow | Rubocop | VS Code',
     description: 'A tic-tac-toe game for the terminal developed in Ruby'
   },
   {
     title: 'Scraper from Yahoo Finance!',
     image: './images/Screenshot-scrapper.png',
+    github: 'https://github.com/Zappat0n/stock_scraper',
     technologies: 'Ruby | Git | Gitflow | Rubocop | VS Code',
     description: 'An application developed with Ruby to get financial information in real-time from Yahoo Finance!.'
   },
@@ -37,6 +39,7 @@ const projects = [
     title: 'Elan Montessori Event website',
     image: './images/Screenshot-elan-event.png',
     technologies: 'HTML | CSS | Bootstrap | Git | Gitflow | StileInt | WebHint | VS Code',
+    github: 'https://github.com/Zappat0n/education2020',
     description: 'This a responsive conference website. It is for a Positive Discipline education event that took place in Murcia (Spain) in 2020.'
   },
   {
