68a5b860bace639cada3fb7e6a0219c4ac106eab
Zappat0n <albarros1973@gmail.com>
2021-03-10 21:57:12 +0100

    Add node

---
+++ src/dist/index.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _project_view = _interopRequireDefault(require("./project_view"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _project_view["default"])(0);
