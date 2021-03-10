b50176be7e7a03d49c70c6df51d7d3a87de876b5
Zappat0n <albarros1973@gmail.com>
2021-03-10 23:24:32 +0100

    Display live button when available

--- src/dist/project_view.dev.js
+++ src/dist/project_view.dev.js
@@ -9,39 +9,22 @@ var _projects = _interopRequireDefault(require("./projects"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-//<h2 class = "border-bottom pb-3">My recent work</h2>
-//<div class="work card mt-5 w-100 my-4">
-//  <div class="card-body">
-//    <div class="row">
-//      <div class="col-12 col-md-6">
-//        <img class="card-img w-100" src="./assets/images/Screenshot-tic-tac-toe.png" alt="Tic-tac-toe image">
-//      </div>
-//      <div class="col-12 col-md-6 p-3">
-//        <h5 class="card-title">Tic-tac-toe game</h5>
-//        <p class="card-text">A tic-tac-toe game for the terminal developed in Ruby</p>
-//        <p class="card-text"><strong>Ruby | Git | Gitflow | Rubocop | VS Code</strong></p>
-//        <form action="./details/detail-tic-tac-toe.html" method="get">
-//          <button class="button m-4" type="submit">
-//            See Project
-//            <span class="button__horizontal"></span>
-//            <span class="button__vertical"></span>
-//          </button>
-//        </form>
-//      </div>
-//    </div>
-//  </div>
-//</div>
-var _default = displayView = function displayView(index) {
+var displayView = function displayView(index) {
   var content = _projects["default"][index];
   document.querySelector('.card-img').setAttribute('src', content.image);
   document.querySelector('.card-title').textContent = content.title;
   document.querySelector('.card-description').textContent = content.description;
   document.querySelector('.card-technologies').textContent = content.technologies;
   document.querySelector('.form-project-github').setAttribute('action', content.github);
+  console.log(content.live);
 
   if (content.live) {
     document.querySelector('.form-project-live').setAttribute('action', content.live);
+    document.querySelector('.div-live').classList.remove('d-none');
+  } else {
+    document.querySelector('.div-live').classList.add('d-none');
   }
 };
 
+var _default = displayView;
 exports["default"] = _default;
--- src/project_view.js
+++ src/project_view.js
@@ -8,8 +8,12 @@ const displayView = (index) => {
   document.querySelector('.card-description').textContent = content.description;
   document.querySelector('.card-technologies').textContent = content.technologies;
   document.querySelector('.form-project-github').setAttribute('action', content.github);
+  console.log(content.live);
   if (content.live) {
     document.querySelector('.form-project-live').setAttribute('action', content.live);
+    document.querySelector('.div-live').classList.remove('d-none');
+  } else {
+    document.querySelector('.div-live').classList.add('d-none');
   }
 
 }
