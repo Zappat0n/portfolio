45ba71f4bc730f6d1f34c237b1884ccd59509fed
Zappat0n <albarros1973@gmail.com>
2021-03-11 09:06:30 +0100

    Reorder and clean code

--- src/dist/project_view.dev.js
+++ src/dist/project_view.dev.js
@@ -1,20 +1,29 @@
-import projects from './projects';
+"use strict";
 
-const displayView = (index) => {
-  const content = projects[index];
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
 
+var _projects = _interopRequireDefault(require("./projects"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var displayView = function displayView(index) {
+  var content = _projects["default"][index];
   document.querySelector('.card-img').setAttribute('src', content.image);
   document.querySelector('.card-title').textContent = content.title;
   document.querySelector('.card-description').textContent = content.description;
   document.querySelector('.card-technologies').textContent = content.technologies;
   document.querySelector('.form-project-github').setAttribute('action', content.github);
+
   if (content.live) {
     document.querySelector('.form-project-live').setAttribute('action', content.live);
     document.querySelector('.div-live').classList.remove('d-none');
   } else {
     document.querySelector('.div-live').classList.add('d-none');
   }
+};
 
-}
-
-export default displayView;
+var _default = displayView;
+exports["default"] = _default;
